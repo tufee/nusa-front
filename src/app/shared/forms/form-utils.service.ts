e3855a79d022f7301ad6566f4d09367a23eabd57
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-18 03:50:58 -0300

    adiciona classe formUtils

---
+++ src/app/shared/forms/form-utils.service.ts
@@ -0,0 +1,49 @@
+
+import { Injectable } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormUtilsService {
+
+  constructor() { }
+
+  validateAllFormFields(formGroup: UntypedFormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+
+      if (control instanceof UntypedFormControl) {
+        control.markAsTouched({ onlySelf: true });
+      }
+
+      if (control instanceof UntypedFormGroup) {
+        control.markAsTouched({ onlySelf: true });
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+
+  getErrorMessage(formGroup: UntypedFormGroup, fieldName: string) {
+    const field = formGroup.get(fieldName) as UntypedFormControl;
+    return this.getErrorMessageFromField(field);
+  }
+
+  getErrorMessageFromField(field: UntypedFormControl) {
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 11;
+      return `Campo deve ter no mínimo ${requiredLength} caracteres`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 11;
+      return `Campo deve ter no máximo ${requiredLength} caracteres`;
+    }
+
+    return 'Campo inválido'
+  }
+}
