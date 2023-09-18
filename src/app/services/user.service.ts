3b67285bc985aba2bb516e6382b1d234eef72612
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-18 03:55:44 -0300

    refatora user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from '../models/user';
+import { Doctor } from '../models/doctor';
+import { Patient } from '../models/patient';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,16 @@ export class UserService {
   private readonly API = 'http://localhost:3000';
 
   constructor(private httpClient: HttpClient) { }
-  savePaciente(user: User) {
-    if (user.tipo == 'paciente') {
-      return this.httpClient.post<User>(`${this.API}/create/paciente`, user);
-    }
-    return this.httpClient.post<User>(`${this.API}/create/medico`, user);
+
+  savePatient(user: Patient) {
+    return this.httpClient.post<Patient>(`${this.API}/create/paciente`, user);
   }
 
-  login(user: Pick<User, 'cpf' | 'senha'>) {
-    return this.httpClient.post<User>(`${this.API}/login`, user);
+  getAllPatient() {
+    return this.httpClient.get<Patient[]>(`${this.API}/paciente`);
+  }
+
+  saveDoctor(doctor: Doctor) {
+    return this.httpClient.post<Doctor>(`${this.API}/create/medico`, doctor);
   }
 }
