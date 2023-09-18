c2400a5856b3461f6a9fa70678c28f43e6cc6b28
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-18 04:03:46 -0300

    adiciona rotas e autenticação das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PatientComponent } from './components/patient/patient.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { AuthenticateUserService } from './services/authenticate-user.service';
 
 const routes: Routes = [
-  { path: '', component: SigninComponent },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
   { path: 'signup', component: SignupComponent },
+  { path: 'login', component: SigninComponent },
+
+  {
+    path: 'home', component: HomeComponent,
+    canActivate: [() => {
+      return inject(AuthenticateUserService).verifyLogin();
+    }],
+  },
+  {
+    path: 'patient', component: PatientComponent,
+    canActivate: [() => {
+      return inject(AuthenticateUserService).verifyLogin();
+    }],
+  },
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
