389e428f315ada825cd8ed9a36bb61b5749d26b2
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-22 04:41:57 -0300

    adiciona interface recipeRequest e recipePayload

--- src/app/models/recipe.ts
+++ src/app/models/recipe.ts
@@ -4,3 +4,21 @@ export interface Recipe {
   medicamento: string;
   data_prescricao: Date;
 }
+
+export interface RecipeRequest {
+  medicamento: {
+    id: string;
+  }
+  paciente: {
+    id: string;
+  },
+  userId: string;
+  data_prescricao: Date;
+}
+
+export interface RecipePayLoad {
+  data_prescricao: Date;
+  nome_medico: string;
+  nome_paciente: string;
+  nome_medicamento: string;
+}
