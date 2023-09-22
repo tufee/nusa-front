3520a7de4c8f69f16e22233c4d74af13fa64ddab
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-22 04:43:21 -0300

    refatora funções saveRecipe e getAllRecipes

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Recipe } from '../models/recipe';
+import { Recipe, RecipePayLoad, RecipeRequest } from '../models/recipe';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +11,23 @@ export class RecipeService {
 
   constructor(private httpClient: HttpClient) { }
 
-  saveRecipe(recipe: Recipe) {
-    return this.httpClient.post<Recipe>(`${this.API}/create/receita`, recipe);
+  saveRecipe(recipe: RecipeRequest) {
+    const medico_id = recipe.userId
+    const paciente_id = recipe.paciente.id
+    const medicamento_id = recipe.medicamento.id
+    const data_prescricao = recipe.data_prescricao
+
+    const payload = {
+      medico_id,
+      paciente_id,
+      medicamento_id,
+      data_prescricao
+    }
+
+    return this.httpClient.post<Recipe>(`${this.API}/create/receita`, payload);
   }
 
-  getAllMedicine() {
-    return this.httpClient.get<Recipe[]>(`${this.API}/receita`);
+  getAllRecipes() {
+    return this.httpClient.get<RecipePayLoad[]>(`${this.API}/receita`);
   }
 }
